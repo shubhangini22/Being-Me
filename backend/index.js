eb2987aa6863fda84d46307c421a1024e58c494d
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-15 10:37:10 +0530

    improvements

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -5,27 +6,19 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
+const app = express();
 
-
-//importing post routes
-
-const app=express();
-
-app.use('./posts',postRoutes);
-app.use(bodyParser.json({limit:"300mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"300mb",extended:true}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://bunny22:bunny123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://bunny22:bunny123@cluster0.kszuc0g.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
-
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
-
-//to connect with database mongodb
