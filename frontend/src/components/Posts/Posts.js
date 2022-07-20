5d11bc641b6d7478b0e06996c654788abd4403c2
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-20 14:41:45 +0530

    files added

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Post from './Post/Post';
 
-
+import useStyle from './styles';
 const Posts = () => {
+    const classes = useStyles();
     return (
        <>
         <h1>POSTS</h1>
