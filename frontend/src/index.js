c42754da64340c199dec2299977cf8cfcf036230
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-20 21:11:48 +0530

    files added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import { Provider } from 'react-redux'
-import {legacy_createStore, applyMiddleware,compose} from 'redux';
+import {legacy_createStore,applyMiddleware,compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
@@ -9,4 +9,10 @@ import App from './App';
 
 const store=createStore(reducers,compose(applyMiddleware(thunk)))
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App/>
+</Provider>,
+
+document.getElementById('root')
+);
