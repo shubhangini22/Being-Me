968fa90fb9cba8a997cb4b4f93e16857d672c2cb
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-21 20:54:10 +0530

    bugs

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,20 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-
+import dotenv from 'dotenv';
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://bunny22:bunny123@cluster0.kszuc0g.mongodb.net/?retryWrites=true&w=majority'
+//const CONNECTION_URL = 'mongodb+srv://bunny22:bunny123@cluster0.kszuc0g.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+//mongoose.set("useAndModify", false);
